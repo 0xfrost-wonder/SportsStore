209a74304a0efa07a61fa92e26408217bed63fff
ICEBLAST <147685893+COOLICEBLAST@users.noreply.github.com>
2023-10-14 18:50:07 -0700

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'app',
+  template: `<div class="bg-success p-2 text-center text-white">
+                This is SportsStore
+              </div>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
